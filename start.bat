6bc734d33fca226201fbcf87714f37e63e50b482
EuphoricPenguin <nxzsizya@20boxme.org>
2018-01-23 19:09:54 -0500

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,8 +1,49 @@
 @echo off
 color 0a
-:start
-cd C:\Users\%username%\Downloads\Moomoo-AI-Bot-Sender-master\Moomoo-AI-Bot-Sender-master
 title MooMoo Bot Sender W/ AI
+cd %CD%
+::: %#((#(##%%%&&&&&&&@@@@@@@@@@@
+::: ##(##%%%%%&&&&@@@@@&@@@@@@@@@
+::: ##%%%%%%%%%&&@@@@@&&%&&&&@@@@
+::: ###%%%&&&&&&&&@&&&&&%%%&&&&@@
+::: %##%%&&.   *@&&&&&.  .&%%&&&&
+::: %%%%&&&.     %&&@@.  .@@@&@@@
+::: &&&&&&&.  .   ,@@@.  .@@@@@@@
+::: &&&&&&&.  .&%   ,@,  .@@@@@@@
+::: &&&&&&&.  .&&@.   .  .@@@@@@@
+::: &&&&&&&.  .&&@@&.    .@@@@@@@
+::: &&&&&%%.  .@@@@@@,   .@@@@@@@
+::: %%%%%%%##%%@@@@@@@@#@@@@@@@@@
+::: %%%%%%%%%&&@@&&&&&&@@@@@@@@@@
+::: &%%%%%%%%%&&&&&&&@@&&@@@@@@@@
+for /f "delims=: tokens=*" %%A in ('findstr /b ::: "%~f0"') do @echo(%%A
+echo MooMoo Bot Sender Starter
+echo -------------------------
+timeout /t 4
+cls
+:start
+echo Use Probe for connecting bots w/o using a server link?
+echo 1 = YES
+echo 2 = NO
+set /p probe=You:
+if %probe%==1 goto probe
+if %probe%==2 goto cont
+:probe
+cls
+echo Please enter in tribe name:
+set /p probetribe= You:
+cls
+echo Please enter in player name:
+set /p probename= You:
+cls
+node index.js --probeTribe %probetribe% --probeName %probename%
+echo ----------
+echo Please copy the applicable party link.
+pause;
+cls
+goto cont
+:cont
+cls
 echo Enter party link:
 set /p partylink=You:
 cls
@@ -27,6 +68,3 @@ if %aionoff%==1 goto aion
 if %aionoff%==2 goto aioff
 :aion
 node index.js --num %numbots% --link %partylink% --tribe %tribename% --name %botnick% --chat %chatmsg% --ai true
-
-:aioff
-node index.js --num %numbots% --link %partylink% --tribe %tribename% --name %botnick% --chat %chatmsg% --ai false
