4eadce10c7e61ecf88de0c31144163500fcf977e
Mega-Mewthree <Mega-Mewthree@users.noreply.github.com>
2018-01-28 00:16:15 -0800

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,70 +1,56 @@
 @echo off
+cls
 color 0a
 title MooMoo Bot Sender W/ AI
 cd %CD%
-::: %#((#(##%%%&&&&&&&@@@@@@@@@@@
-::: ##(##%%%%%&&&&@@@@@&@@@@@@@@@
-::: ##%%%%%%%%%&&@@@@@&&%&&&&@@@@
-::: ###%%%&&&&&&&&@&&&&&%%%&&&&@@
-::: %##%%&&.   *@&&&&&.  .&%%&&&&
-::: %%%%&&&.     %&&@@.  .@@@&@@@
-::: &&&&&&&.  .   ,@@@.  .@@@@@@@
-::: &&&&&&&.  .&%   ,@,  .@@@@@@@
-::: &&&&&&&.  .&&@.   .  .@@@@@@@
-::: &&&&&&&.  .&&@@&.    .@@@@@@@
-::: &&&&&%%.  .@@@@@@,   .@@@@@@@
-::: %%%%%%%##%%@@@@@@@@#@@@@@@@@@
-::: %%%%%%%%%&&@@&&&&&&@@@@@@@@@@
-::: &%%%%%%%%%&&&&&&&@@&&@@@@@@@@
-for /f "delims=: tokens=*" %%A in ('findstr /b ::: "%~f0"') do @echo(%%A
-echo MooMoo Bot Sender Starter
-echo -------------------------
-timeout /t 4
 cls
+@echo off
 :start
-echo Use Probe for connecting bots w/o using a server link?
-echo 1 = YES
-echo 2 = NO
-set /p probe=You:
-if %probe%==1 goto probe
-if %probe%==2 goto cont
+echo Menu:
+echo     1. Bot Sender
+echo     2. Prober
+set /p menu="Select an option: "
+if %menu%==2 goto probe
+goto cont
 :probe
 cls
-echo Please enter in tribe name:
-set /p probetribe= You:
+echo Probe by:
+echo     1. Tribe name
+echo     2. Player name
+set /p probetype="Type: "
 cls
-echo Please enter in player name:
-set /p probename= You:
+if %probetype%==1 goto probebytribe
+goto probebyname
+:probebytribe
+set /p probetribe="Please enter the tribe to search for: "
 cls
-node index.js --probeTribe %probetribe% --probeName %probename%
+node index.js --probeTribe %probetribe%
+goto endprobe
+:probebyname
+set /p probename="Please enter the player to search for: "
+cls
+node index.js --probeName %probename%
+goto endprobe
+:endprobe
 echo ----------
 echo Please copy the applicable party link.
 pause;
 cls
-goto cont
+goto start
 :cont
 cls
-echo Enter party link:
-set /p partylink=You:
+set /p partylink="Party Link: "
+set /p tribename="Tribe Name: "
+set /p botnick="Bot Name: "
+set /p numbots="Number of Bots: "
+set /p chatmsg="Bot Chat Message: "
+set /p ai="AI? (Y/N): "
+set /p autoheal="Autoheal? (Y/N): "
 cls
-echo Enter Tribe Name:
-set /p tribename=You:
-cls
-echo Nickname for Bots:
-set /p botnick=You:
-cls
-echo Number of Bots:
-set /p numbots=You:
-cls
-echo Bot Chat Message:
-set /p chatmsg=You:
-cls
-echo AI ON or OFF?
-echo 1 = ON
-echo 2 = OFF
-set /p aionoff=You:
-cls
-if %aionoff%==1 goto aion
-if %aionoff%==2 goto aioff
-:aion
-node index.js --num %numbots% --link %partylink% --tribe %tribename% --name %botnick% --chat %chatmsg% --ai true
+if /I %ai%==Y set ai="true" & goto checkautoheal
+set ai="false"
+:checkautoheal
+if /I %autoheal%==N set autoheal="false" & goto exec
+set autoheal="true"
+:exec
+node index.js --num %numbots% --link %partylink% --tribe %tribename% --name %botnick% --chat %chatmsg% --ai %ai% --autoheal %autoheal%
